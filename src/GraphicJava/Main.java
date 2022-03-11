4ab2ac39ea91ed1b9115b53a1e1a08bbd4f11e74
Haytham Salama <haythamasalama@gmail.com>
2022-03-11 17:28:12 +0200

    add Lines class to main

--- src/GraphicJava/Main.java
+++ src/GraphicJava/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("welcome to graphic ...");
+        // drawLine method example
+        new JFrameProject(new Lines(), "lines");
+
     }
 }

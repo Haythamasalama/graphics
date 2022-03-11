9f7b2005ed5106c8708e947b3892139667cced4b
Haytham Salama <haythamasalama@gmail.com>
2022-03-11 18:16:27 +0200

    add Oval to main

--- src/Oval/Oval.java
+++ src/Oval/Oval.java
@@ -2,13 +2,25 @@ package Oval;
 import GraphicJava.JFrameProject;
 import GraphicJava.JPanelProject;
 import java.awt.*;
+
 public class Oval  extends JPanelProject {
+
+
     public void oval(Graphics g){
         g.setColor(Color.red);
         g.drawOval(80,80,200,300);
     }
+
     public void circle(Graphics g){
         g.setColor(Color.red);
         g.drawOval(80,80,300,300);
     }
+
+    public static void main(String[] args) {
+        System.out.println("welcome to graphic ---> Oval");
+        // drawOval method example
+        new JFrameProject(new Oval(), "Oval");
+
+    }
+
 }

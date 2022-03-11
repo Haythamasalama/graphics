2e4e7179fb33931bda169167472a1eef563fd4fd
Haytham Salama <haythamasalama@gmail.com>
2022-03-11 18:15:42 +0200

    draw circle and oval

--- src/Oval/Oval.java
+++ src/Oval/Oval.java
@@ -3,4 +3,12 @@ import GraphicJava.JFrameProject;
 import GraphicJava.JPanelProject;
 import java.awt.*;
 public class Oval  extends JPanelProject {
+    public void oval(Graphics g){
+        g.setColor(Color.red);
+        g.drawOval(80,80,200,300);
+    }
+    public void circle(Graphics g){
+        g.setColor(Color.red);
+        g.drawOval(80,80,300,300);
+    }
 }

4bcad03a8fbe2389582a3a4a005eae5c99a5355c
Haytham Salama <haythamasalama@gmail.com>
2022-03-11 18:39:52 +0200

    draw example3

--- src/Oval/Oval.java
+++ src/Oval/Oval.java
@@ -16,6 +16,21 @@ public class Oval  extends JPanelProject {
         g.drawOval(80,80,300,300);
     }
 
+    public void example3(Graphics g){
+        g.setColor(Color.decode("#B3E5FC"));
+        g.fillRect(20,20,300,300);
+        // draw circle 1 and 2
+        g.setColor(Color.decode("#03A9F4"));
+        g.fillOval(20,20,100,100);
+        g.fillOval(220,20,100,100);
+        // draw circle 3
+        g.setColor(Color.decode("#0288D1"));
+        g.fillOval(120,120,100,100);
+        // draw circle 4 and 5
+        g.setColor(Color.decode("#03A9F4"));
+        g.fillOval(20,220,100,100);
+        g.fillOval(220,220,100,100);
+    }
     @Override
     public void paint(Graphics g) {
         super.paint(g);

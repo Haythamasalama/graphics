9216d8a50ac051e229e0a6f438284d51cbc34832
Haytham Salama <haythamasalama@gmail.com>
2022-03-11 22:51:02 +0200

    add example 3 ,4 ,5 to paint and format code

--- src/Oval/Oval.java
+++ src/Oval/Oval.java
@@ -1,35 +1,38 @@
 package Oval;
+
 import GraphicJava.JFrameProject;
 import GraphicJava.JPanelProject;
+
 import java.awt.*;
 
-public class Oval  extends JPanelProject {
+public class Oval extends JPanelProject {
 
 
-    public void oval(Graphics g){
+    public void oval(Graphics g) {
         g.setColor(Color.red);
-        g.drawOval(80,80,200,300);
+        g.drawOval(80, 80, 200, 300);
     }
 
-    public void circle(Graphics g){
+    public void circle(Graphics g) {
         g.setColor(Color.red);
-        g.drawOval(80,80,300,300);
+        g.drawOval(80, 80, 300, 300);
     }
 
-    public void example3(Graphics g){
+    public void example3(Graphics g) {
         g.setColor(Color.decode("#B3E5FC"));
-        g.fillRect(20,20,300,300);
+        g.fillRect(20, 20, 300, 300);
         // draw circle 1 and 2
         g.setColor(Color.decode("#03A9F4"));
-        g.fillOval(20,20,100,100);
-        g.fillOval(220,20,100,100);
+        g.fillOval(20, 20, 100, 100);
+        g.fillOval(220, 20, 100, 100);
         // draw circle 3
         g.setColor(Color.decode("#0288D1"));
-        g.fillOval(120,120,100,100);
+        g.fillOval(120, 120, 100, 100);
         // draw circle 4 and 5
         g.setColor(Color.decode("#03A9F4"));
-        g.fillOval(20,220,100,100);
-        g.fillOval(220,220,100,100);
+        g.fillOval(20, 220, 100, 100);
+        g.fillOval(220, 220, 100, 100);
+    }
 
     public void example4(Graphics g) {
         for (int i = 15; i <= 400; i += 10) {
@@ -52,6 +55,9 @@ public class Oval  extends JPanelProject {
         super.paint(g);
         this.circle(g);
         this.oval(g);
+        this.example3(g);
+        this.example4(g);
+        this.example5(g);
     }
 
     public static void main(String[] args) {

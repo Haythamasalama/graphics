63eecfdab721f0f191839d308ab74eb657122d32
Haytham Salama <haythamasalama@gmail.com>
2022-03-12 00:41:51 +0200

    draw example 5

--- src/Lines/Lines.java
+++ src/Lines/Lines.java
@@ -31,12 +31,26 @@ public class Lines extends JPanelProject {
         g.drawLine(200,150,200,300);
     }
 
+    public void example5(Graphics g){
+        g.setColor(Color.red);
+        int x ,y ,r = 100, xc = 250 , yc = 250;
+        g.drawLine(250,0,250,500);
+        g.drawLine(0,250,500,250);
+
+        for (int a=0 ; a<= 360 ;a++){
+            x = (int) Math.round(r* Math.cos((a * Math.PI)/180));
+            y = (int) Math.round(r* Math.sin((a * Math.PI)/180));
+            g.drawLine(xc + x, yc - y, xc + x, yc - y);
+        }
+    }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         this.rectangle(g);
         this.triangle(g);
         this.hLetters(g);
+        this.example5(g);
     }
 
     public static void main(String[] args) {

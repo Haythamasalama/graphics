a647585494009326e9d77f866550439fb9f24071
Haytham Salama <haythamasalama@gmail.com>
2022-03-11 17:07:49 +0200

    draw rectangle

--- src/GraphicJava/Lines.java
+++ src/GraphicJava/Lines.java
@@ -3,4 +3,12 @@ package GraphicJava;
 import java.awt.*;
 
 public class Lines extends JPanelProject {
+
+    public void rectangle(Graphics g){
+        g.setColor(Color.red);
+        g.drawLine(10,10,200,10);
+        g.drawLine(10,10,10,200);
+        g.drawLine(10,200,200,200);
+        g.drawLine(200,10,200,200);
+    }
 }

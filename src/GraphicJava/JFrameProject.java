0390e0987b520b28ee8f68612801df3aaf45e7f2
Haytham Salama <haythamasalama@gmail.com>
2022-03-11 20:19:23 +0200

    update title

--- src/GraphicJava/JFrameProject.java
+++ src/GraphicJava/JFrameProject.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class JFrameProject extends JFrame {
 
     public JFrameProject(JPanelProject panel ,String title)  {
-        super("graphic java" + title);
+        super("graphic java - " + title);
         this.add(panel);
         this.pack();
         this.setLocationRelativeTo(null);

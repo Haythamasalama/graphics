dd51a0e58eafb35ed1bcb5824a42d021b1e2e56e
Haytham Salama <haythamasalama@gmail.com>
2022-04-08 18:09:03 +0200

    add randomColor method

--- src/Helpers/Helpers.java
+++ src/Helpers/Helpers.java
@@ -4,4 +4,15 @@ import java.awt.*;
 
 public class Helpers {
 
+    /**
+     * generate a random color
+     * @return Color
+     */
+    public Color randomColor() {
+        int r = (int) (Math.random() * 256);
+        int g = (int) (Math.random() * 256);
+        int b = (int) (Math.random() * 256);
+
+        return new Color(r, g, b);
+    }
 }

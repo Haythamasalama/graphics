b384cb56722ce6ef1b0c8027d16116293312ee74
Haytham Salama <haythamasalama@gmail.com>
2022-04-09 23:43:42 +0200

    edit width calculation

--- src/Animations/Car.java
+++ src/Animations/Car.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionListener;
 class Car extends JPanelProject implements ActionListener {
 
     Timer timer;
-    int x = 50, y = 50, width = 120;
+    int x = 50, y = 50, width = 180;
 
     public Car() {
         timer = new Timer(100, this);
@@ -24,9 +24,9 @@ class Car extends JPanelProject implements ActionListener {
         g.drawLine(x + 10, y, x + 20, y - 20);
         g.drawLine(x + 20, y - 20, x + (width - 30), y - 20);
         g.drawLine(x + (width - 30), y - 20, x + (width - 20), y);
-        g.drawRect(x, y, width, 10);
-        g.drawOval(x + 10, y + 10, 15, 15);
-        g.drawOval(width - ((width / 3) - x), y + 10, 15, 15);
+        g.drawRect(x, y, width, 15);
+        g.drawOval(x + 20, y + 15,(int) (width*0.1), (int) (width*0.1));
+        g.drawOval(x + (int) (width/1.5), y + 15, (int) (width*0.1), (int) (width*0.1));
     }
 
     @Override

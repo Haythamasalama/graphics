046adb3aba6a824f49b022a6c12b085efd323be4
Haytham Salama <haythamasalama@gmail.com>
2022-04-11 02:58:44 +0200

    add rectangle function

--- src/Polygon/Polygons.java
+++ src/Polygon/Polygons.java
@@ -6,6 +6,28 @@ import java.awt.*;
 
 public class Polygons extends JPanelProject {
 
+
+    public void rectangle(Graphics g) {
+        // points : (100,100) , (200,100) ,(200,150) ,(100,150)
+        g.drawPolygon(
+                new int[]{100, 200, 200, 100},
+                new int[]{100, 100, 150, 150},
+                4
+        );
+
+        /*
+         * or you can use different way to draw polygon
+         * 1- make new object polygon
+         * 2- use add point method
+         * 3- use draw polygon  method
+         */
+        Polygon rec = new Polygon();
+        rec.addPoint(200,200);
+        rec.addPoint(300,200);
+        rec.addPoint(300,250);
+        rec.addPoint(200,250);
+        g.drawPolygon(rec);
+    }
     @Override
     public void paint(Graphics g) {
     }

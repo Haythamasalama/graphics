5ac13f053a146fa70f5b69a4585c018906767bd1
Haytham Salama <haythamasalama@gmail.com>
2022-06-13 17:20:41 +0300

    add example2

--- src/Graphics2D/Graphic2D.java
+++ src/Graphics2D/Graphic2D.java
@@ -22,6 +22,18 @@ public class Graphic2D extends JPanelProject {
         g2d.rotate(90 * Math.PI / 180, 100, 100);
         g2d.drawPolygon(p);
     }
+
+    public void example2(Graphics g) {
+        g2d = (Graphics2D) g;
+        Polygon p = new Polygon(
+                new int[]{100, 200, 200, 100, 100},
+                new int[]{100, 100, 150, 150, 100},
+                5
+        );
+        g2d.drawPolygon(p);
+        Helpers.scale(0.5, 0.5, 100, 100, g2d);
+        g2d.drawPolygon(p);
+    }
     public static void main(String[] args) {
         System.out.println("welcome to graphic ---> Graphic2D");
         new JFrameProject(new Graphic2D(), "Polygon");

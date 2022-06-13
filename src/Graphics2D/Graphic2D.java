6d6f8e8833356eb313e0be70d8b1c0ece54eabeb
Haytham Salama <haythamasalama@gmail.com>
2022-06-13 17:20:23 +0300

    add example1

--- src/Graphics2D/Graphic2D.java
+++ src/Graphics2D/Graphic2D.java
@@ -10,6 +10,18 @@ import java.awt.geom.AffineTransform;
 public class Graphic2D extends JPanelProject {
     Graphics2D g2d;
     Graphics g;
+
+    public void example1(Graphics g) {
+        g2d = (Graphics2D) g;
+        Polygon p = new Polygon(
+                new int[]{100, 200, 200, 100, 100},
+                new int[]{100, 100, 150, 150, 100},
+                5
+        );
+        g2d.drawPolygon(p);
+        g2d.rotate(90 * Math.PI / 180, 100, 100);
+        g2d.drawPolygon(p);
+    }
     public static void main(String[] args) {
         System.out.println("welcome to graphic ---> Graphic2D");
         new JFrameProject(new Graphic2D(), "Polygon");
